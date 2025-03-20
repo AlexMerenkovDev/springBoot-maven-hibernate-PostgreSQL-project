fcf37d38187dfd4b8f971035a2390adc02c918d4
Alexandre J. H. Merenkov Amade <aamade@cedsif.gov.mz>
2025-03-20 18:36:48 +0200

    Primeira classe de database

--- src/main/java/com/challenge/library/LibraryApplication.java
+++ src/main/java/com/challenge/library/LibraryApplication.java
@@ -1,13 +1,30 @@
 package com.challenge.library;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+@Log
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public LibraryApplication(final DataSource dataSource){
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		log.info("DataSource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute(("select 1"));
+	}
 }
