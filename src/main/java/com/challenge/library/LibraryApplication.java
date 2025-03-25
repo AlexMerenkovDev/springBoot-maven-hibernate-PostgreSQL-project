c38609a64aad5bb964cb7f7bd47a77e48d355463
Alexandre J. H. Merenkov Amade <aamade@cedsif.gov.mz>
2025-03-25 17:41:46 +0200

    JDBCTemplate Setup (DONE)

--- src/main/java/com/challenge/library/LibraryApplication.java
+++ src/main/java/com/challenge/library/LibraryApplication.java
@@ -1,30 +1,16 @@
 package com.challenge.library;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 @Log
 @SpringBootApplication
-public class LibraryApplication implements CommandLineRunner {
-
-	private final DataSource dataSource;
-
-	public LibraryApplication(final DataSource dataSource){
-		this.dataSource = dataSource;
-	}
+public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
-		log.info("DataSource: " + dataSource.toString());
-		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
-		restTemplate.execute("select 1");
-	}
 }
